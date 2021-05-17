d4027247ae160fb02fb7963e83db5564cf6da5b1
David Bouscarle <d.bouscarle@gmail.com>
2021-05-17 14:34:56 +0200

    Add routing symfony composer and create some routes

--- public/index.php
+++ public/index.php
@@ -2,30 +2,35 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$response = new Response();
+$context = new RequestContext();
+$context->fromRequest($request);
 
-$pathInfo = $request->getPathInfo();
+$routes = require __DIR__ . '/../src/routes.php';
 
-$map = [
-    '/hello'    => 'hello.php',
-    '/bye'      => 'bye.php'
-];
+$urlMatcher = new UrlMatcher($routes, $context);
 
-if (isset($map[$pathInfo]))
+try
 {
-    extract($request->query->all());
+    $result = $urlMatcher->match($request->getPathInfo());
+    extract($result);
+
     ob_start();
-    include __DIR__ . '/../src/pages/' . $map[$pathInfo];
-    $response->setContent(ob_get_clean());
-} else
+    include __DIR__ . '/../src/pages/' . $result['_route'] . '.php';
+    $response = new Response(ob_get_clean());
+} catch (ResourceNotFoundException $error)
 {
-    $response->setContent('La page demandée n\'existe pas');
-    $response->setStatusCode(404);
+    $response = new Response('La page demandée n\'existe pas', 404);
+} catch (Exception $error)
+{
+    $response = new Response('Une erreur serveur est survenue', 500);
 }
 
 $response->send();
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,8 +1,1 @@
-<?php
-
-use Symfony\Component\HttpFoundation\Response;
-
-$response = new Response();
-$response->setContent(sprintf('Hello %s', htmlspecialchars(isset($_GET['name']) ? $name : 'World', ENT_QUOTES)));
-
-$response->send();
+Hello <?= htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES) ?>
